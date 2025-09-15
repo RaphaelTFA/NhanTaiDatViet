0d714d7374c15388c305268c39ddf2191526a85c
RaphaelTFA <hungna200111@gmail.com>
2025-09-15 13:22:56 +0700

    feat: add example configuration file

---
+++ config.example.py
@@ -0,0 +1,16 @@
+# Setting
+TEST_MODE = False
+GEN_PROMPT = False
+
+# NEO4J config
+NEO4J_URI = "neo4j://127.0.0.1:7687"
+NEO4J_USER = "neo4j"
+NEO4J_PASSWORD = ""
+
+#Canvas config
+CANVAS_URL = "https://something.instructure.com"
+CANVAS_API = ""
+
+# Open-router LLM
+MODEL = "anthropic/claude-sonnet-4"
+LLM_API_KEY = ""
