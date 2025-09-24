371ff9b08214771e505bd0d6617d682d325ea630
RaphaelTFA <hungna200111@gmail.com>
2025-09-24 21:12:40 +0700

    fix: update working directory path and enhance subprocess command for server launch

--- main.example.py
+++ main.example.py
@@ -1,11 +1,11 @@
 import os
 import subprocess
 
-os.chdir(r"...") # direction dẫn tới file NhanTaiDatViet
+os.chdir(r"C:\Users\hungn\projects\Project_1\code\NhanTaiDatViet") 
 subprocess.run([
     "uvicorn", 
-    "MCP_server:app", 
-    "--reload", 
+    "MCP_server:app",
     "--host", "127.0.0.1", 
-    "--port", "8000"
+    "--port", "8000",
+    "--workers", "4"
 ])
